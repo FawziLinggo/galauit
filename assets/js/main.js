048d806d88d868ae8d63d2040ae08c0ed1fe010b
FawziLinggo <fawzilinggo@gmail.com>
2026-04-07 15:33:49 +0700

    add dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,3 +232,25 @@ if (scrollTop) {
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+
+const toggle = document.getElementById("themeToggle");
+
+// load dari localStorage
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggle.textContent = "☀️";
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggle.textContent = "🌙";
+  }
+});
